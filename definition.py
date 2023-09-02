907dd4ac312f2b3dd0c0530badb6e93067411d8f
Halil_Hasmer <halilpython@gmail.com>
2023-09-02 21:20:59 +0300

    self-write Definition.get() method

--- definition.py
+++ definition.py
@@ -1,6 +1,17 @@
+import pandas as pd
+
+
 class Definition:
     def __init__(self, term):
         self.term = term
 
     def get(self):
-        return f"{self.term} means avocado"
+        df = pd.read_csv("data.csv")
+        definitions = df.loc[df["word"] == self.term]["definition"]
+        return definitions
+
+
+df = pd.read_csv("data.csv")
+print(df.head())
+word = "acid"
+print(df.loc[df["word"] == word]["definition"])
